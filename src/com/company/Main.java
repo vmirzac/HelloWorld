4a1b985bdc6417417f992f999bd89ed41e9e05d5
gitVictor <victormarzac@yahoo.com>
2018-01-16 16:07:33 +0200

    3rd Commit.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 	// test comment pulled
-    // test comment pushed
+    // test comment pushed2
         System.out.println("Hello World!");
     }
 }
