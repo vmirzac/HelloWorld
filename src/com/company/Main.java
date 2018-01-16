69931d8d4810d782b390f3355787edcbe73d3f3f
gitVictor <victormarzac@yahoo.com>
2018-01-16 15:48:03 +0200

    Hello World second commit.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-	// test code written
+	// test comment pulled
+    // test comment pushed
         System.out.println("Hello World!");
     }
 }
